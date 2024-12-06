23fe4a6c77ee05b50d9c4e44952251a4296bd84e
Dmy0 <popovdmytro00@gmail.com>
2024-12-07 00:35:19 +0200

    upd nextsent

--- main.js
+++ main.js
@@ -15,11 +15,37 @@
 
 // 2 classa -overlay(opacity 1 transition to opacity 0.4) -hide(display none) class list na body method style body.style.overflow = "hidden" "visable"
 
-let popup = document.querySelector(".header__button-login");
-popup.addEventListener("click", function () {
-  popup.classList.add(".popup__open");
-});
-let popupClose = document.querySelector(".popup__close");
-popupClose.addEventListener("click", function () {
-  popupClose.classList.remove(".popup__open");
-});
+const popupOpenLog = document.querySelector(".header__button-login"),
+  popupLog = document.querySelector(".login"),
+  popup = document.querySelector(".popup__close"),
+  overlay = document.querySelector(".popups");
+
+function toggleClass(btn, overlay, popup, hide, opacity) {
+  btn.addEventListener("click", function (event) {
+    popup.classList.toggle(hide);
+    overlay.classList.toggle(opacity);
+    console.log(event.target);
+  });
+}
+toggleClass(popupOpenLog, popup, popupLog, "hide", "opacity");
+toggleClass(popup, popup, popupLog, "hide", "opacity");
+
+// function documentClosePopup(popup, hide, opacity, overlay) {
+//   document.addEventListener("click", function (e) {
+//     if (!popup.contains(e.target)) {
+//       popup.classList.remove(hide);
+//       overlay.classList.add(opacity);
+//     }
+//   });
+// }
+
+function documentClosePopup(popup, hide, opacity, overlay) {
+  document.addEventListener("click", function (e) {
+    if (!popup.contains(e.target)) {
+      // Проверяем, находится ли клик вне popup
+      popup.classList.add(hide); // Скрываем popup
+      overlay.classList.remove(opacity); // Убираем затемнение
+    }
+  });
+}
+// documentClosePopup(popup, overlay, "hide", "opacity");
