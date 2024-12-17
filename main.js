17dd0e270a9484df77a5fb1fc424dbf519055997
Dmy0 <popovdmytro00@gmail.com>
2024-12-17 16:01:14 +0200

    upd nextsent

--- main.js
+++ main.js
@@ -11,6 +11,7 @@ popupSign = document.querySelector(".sign");
 // log
 function toggleClass(btn, overlay, popup, hide, opacity) {
   btn.addEventListener("click", function (event) {
+    this.classList.toggle("active");
     popup.classList.toggle(hide);
     overlay.classList.toggle(opacity);
     console.log(event.target);
@@ -32,23 +33,8 @@ documentClosePopup(popupLog, overlay, "hide", "opacity", popupOpenLog);
 const burger = document.querySelector(".header__burger"),
   burgerMenu = document.querySelector(".header__wrap");
 
-function showBurger(btn, menu) {
-  burger.addEventListener("click", function () {
-    btn.classList.toggle("active");
-    menu.classList.toggle("active");
-    document.body.style.overflow = "hidden";
-  });
-}
-
-showBurger(burger, burgerMenu, "active");
+toggleClass(burger, document.body, burgerMenu, "active", "overflow");
 // reg btn
-function toggleClass(btn, overlay, popup, hide, opacity) {
-  btn.addEventListener("click", function (event) {
-    popup.classList.toggle(hide);
-    overlay.classList.toggle(opacity);
-    console.log(event.target);
-  });
-}
 toggleClass(popupOpenReg, overlay, popupSign, "hide", "opacity");
 toggleClass(popupRegClose, overlay, popupSign, "hide", "opacity");
 
@@ -62,23 +48,4 @@ function documentClosePopup(popup, overlay, hide, opacity, btn) {
 }
 documentClosePopup(popupSign, overlay, "hide", "opacity", popupOpenReg);
 
-// sign
-function toggleClass(btn, overlay, popup, hide, opacity) {
-  btn.addEventListener("click", function (event) {
-    popup.classList.toggle(hide);
-    overlay.classList.toggle(opacity);
-    console.log(event.target);
-  });
-}
 toggleClass(popupOpenSign, overlay, popupSign, "hide", "opacity");
-toggleClass(popupSignClose, overlay, popupSign, "hide", "opacity");
-
-function documentClosePopup(popup, overlay, hide, opacity, btn) {
-  document.addEventListener("click", function (e) {
-    if (e.target.contains(popup) && e.target !== btn) {
-      popup.classList.add(hide);
-      overlay.classList.remove(opacity);
-    }
-  });
-}
-documentClosePopup(popupSign, overlay, "hide", "opacity", popupOpenReg);
